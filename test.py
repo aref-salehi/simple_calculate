03b744e68873d6184854e817b25593406f120caa
alirezatd80 <alirezatd80@gmail.com>
2025-07-25 00:31:03 +0330

    edit

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def test_sum():
 def test_subtract():
     assert function.subtract(1,3) == -2
     assert function.subtract(2,2) == 0
-    assert function.subtract(3.-4) == 7
+    assert function.subtract(3,-4) == 7
     assert function.subtract(-5,9) == -14
     
     
